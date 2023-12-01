9e8391a9f3cdbd7dd8aca74278e0ec28da367573
nandobfer <nandobfer@yahoo.com.br>
2023-11-30 22:17:23 -0300

    feat: roomlist style

--- src/components/RoomsList.tsx
+++ src/components/RoomsList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Box } from "@mui/material"
+import { Box, Paper, Skeleton } from "@mui/material"
 import { useIo } from "../hooks/useIo"
 import { Room } from "../definitions/Room"
 
@@ -9,22 +9,23 @@ export const RoomsList: React.FC<RoomsListProps> = ({}) => {
     const io = useIo()
 
     const [rooms, setRooms] = useState<Room[]>([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         io.emit("room:list")
-        console.log('oi')
+        console.log("oi")
 
         io.on("room:list", (data) => {
-            console.log(data)
             setRooms(data)
+            setLoading(false)
         })
 
-        io.on('room:new', (room: Room) => {
-            setRooms(previous => [...previous.filter(item => item.id != room.id), room])
+        io.on("room:new", (room: Room) => {
+            setRooms((previous) => [...previous.filter((item) => item.id != room.id), room])
         })
 
-        io.on('room:remove', (room: Room) => {
-            setRooms(previous => previous.filter(item => item.id != room.id))
+        io.on("room:remove", (room: Room) => {
+            setRooms((previous) => previous.filter((item) => item.id != room.id))
         })
 
         return () => {
@@ -35,12 +36,11 @@ export const RoomsList: React.FC<RoomsListProps> = ({}) => {
     }, [])
 
     return (
-        <Box sx={{}}>
-            <Box sx={{ flexDirection: "column" }}>
-                {rooms.map((room) => (
-                    <Box key={room.id}>{room.name}</Box>
-                ))}
-            </Box>
-        </Box>
+        <Paper sx={{ width: "100%", padding: "2vw 1vw", borderRadius: "0 2vw" }}>
+            {rooms.map((room) => (
+                <Box key={room.id}>{room.name}</Box>
+            ))}
+            {!rooms.length && <Box sx={{ color: "secondary.main" }}>parece que não há nenhuma sala</Box>}
+        </Paper>
     )
 }
