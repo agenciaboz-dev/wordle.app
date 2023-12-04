20aee585f253866ad21063649f3a6e7d28ec220d
nandobfer <nandobfer@yahoo.com.br>
2023-12-04 12:57:20 -0300

    update: backend url

--- src/contexts/ioContext.tsx
+++ src/contexts/ioContext.tsx
@@ -15,8 +15,8 @@ const IoContext = createContext<IoContextValue>({} as IoContextValue)
 
 export default IoContext
 
-const io = ioSocket("ws://localhost:8080")
-// const io = ioSocket("wss://agencyboz.com:8080")
+// const io = ioSocket("ws://localhost:8080")
+const io = ioSocket("wss://agencyboz.com:8080")
 
 export const IoProvider: React.FC<IoProviderProps> = ({ children }) => {
     const { snackbar } = useSnackbar()
