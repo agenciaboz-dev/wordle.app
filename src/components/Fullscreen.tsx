e2ce49cbdcf4b29af4c92a7ef3d0784966557a72
nandobfer <nandobfer@yahoo.com.br>
2023-12-04 18:45:47 -0300

    update: removed fullscren

--- src/components/Fullscreen.tsx
+++ src/components/Fullscreen.tsx
@@ -22,11 +22,12 @@ export const Fullscreen: React.FC<FullscreenProps> = ({}) => {
 
     const icon_style: SxProps = { width: SIZE, height: SIZE }
 
-    return (
-        <Box sx={{ position: "absolute", top: "1vw", right: "1vw" }}>
-            <IconButton color="primary" sx={{}} onClick={handleFullScreen}>
-                {!document.fullscreenElement ? <FullscreenIcon sx={icon_style} /> : <FullscreenExitIcon sx={icon_style} />}
-            </IconButton>
-        </Box>
-    )
+    return null
+    //     (
+    //     <Box sx={{ position: "absolute", top: "1vw", right: "1vw" }}>
+    //         <IconButton color="primary" sx={{}} onClick={handleFullScreen}>
+    //             {!document.fullscreenElement ? <FullscreenIcon sx={icon_style} /> : <FullscreenExitIcon sx={icon_style} />}
+    //         </IconButton>
+    //     </Box>
+    // )
 }
