ec975a9acacb0e362c11480564375a1ff6f22d90
nandobfer <nandobfer@yahoo.com.br>
2023-12-08 12:09:41 -0300

    update: players drawer style

--- src/components/PlayersDrawer/index.tsx
+++ src/components/PlayersDrawer/index.tsx
@@ -40,25 +40,33 @@ export const PlayersDrawer: React.FC<PlayersDrawerProps> = ({ room, player }) =>
                 sx: { width: "100vw", height: "85vh", bgcolor: "background.default", overflow: "visible" }
             }}>
             <Puller bleeding_edge={bleeding_edge} />
-            <Box sx={{ flexDirection: "column", padding: "10vw", height: "100%", paddingTop: 0 }}>
+            <Box sx={{ flexDirection: "column", padding: "0 10vw 5vw 10vw", height: "100%" }}>
                 <Box sx={{ flexDirection: "column", justifyContent: "space-between", height: "100%", gap: "5vw" }}>
                     <WordContainer player={player} room={room} />
-                    {!room.playing &&
-                        (player.id == room.host.id ? (
-                            <Button
-                                variant="contained"
-                                sx={{ borderRadius: "0 5vw", color: "secondary.main", fontWeight: "bold" }}
-                                onClick={handleNextRound}>
-                                começar próximo round
-                            </Button>
-                        ) : (
-                            <Paper
-                                sx={{ borderRadius: "0 10vw", bgcolor: "warning.main", color: "secondary.main", padding: "5vw", fontWeight: "bold" }}>
-                                esperando o host começar
-                            </Paper>
-                        ))}
                     <PlayersList players={room.players.filter((item) => item != player)} player={player} room={room} />
-                    <MenusButtons player={player} room={room} />
+                    <Box sx={{ marginTop: "auto", flexDirection: "column", width: "100%", gap: "5vw" }}>
+                        {!room.playing &&
+                            (player.id == room.host.id ? (
+                                <Button
+                                    variant="contained"
+                                    sx={{ borderRadius: "0 5vw", color: "secondary.main", fontWeight: "bold" }}
+                                    onClick={handleNextRound}>
+                                    começar próximo round
+                                </Button>
+                            ) : (
+                                <Paper
+                                    sx={{
+                                        borderRadius: "0 10vw",
+                                        bgcolor: "warning.main",
+                                        color: "secondary.main",
+                                        padding: "5vw",
+                                        fontWeight: "bold"
+                                    }}>
+                                    esperando o host começar
+                                </Paper>
+                            ))}
+                        <MenusButtons player={player} room={room} />
+                    </Box>
                 </Box>
             </Box>
         </SwipeableDrawer>
