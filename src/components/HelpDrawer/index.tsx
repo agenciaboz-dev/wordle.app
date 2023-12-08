01898f86c7060c593188bd55094ab71f5fb8b296
nandobfer <nandobfer@yahoo.com.br>
2023-12-08 12:05:31 -0300

    update: tutorial

--- src/components/HelpDrawer/index.tsx
+++ src/components/HelpDrawer/index.tsx
@@ -46,46 +46,44 @@ export const HelpDrawer: React.FC<HelpDrawerProps> = ({}) => {
             // slotProps={{ backdrop: { sx: backdropStyle } }}
             PaperProps={{
                 elevation: 5,
-                sx: { width: "100vw", height: "90vh", bgcolor: "background.default", borderRadius: "0 0 10vw 10vw" }
+                sx: { width: "100vw", height: "85vh", bgcolor: "background.default", borderRadius: "0 0 10vw 10vw" }
             }}>
-            <Box sx={{ flexDirection: "column", padding: "10vw", height: "100%" }}>
-                <Box sx={{ flexDirection: "column", height: "100%", gap: "5vw" }}>
-                    <Box sx={{ fontSize: "1.5rem", fontWeight: "bold", alignSelf: "center" }}>como jogar</Box>
-                    <Box>o objetivo do jogo é descobrir qual é a palavra</Box>
-                    <Box>digite uma palavra com a quantidade de letras equivalente para receber dicas</Box>
-                    <Box sx={{ gap: "2vw" }}>
-                        <Letter letter="T" />
-                        <Letter letter="R" />
-                        <Letter letter="E" />
-                        <Letter letter="T" />
-                        <Letter letter="A" partial />
-                    </Box>
-                    <Box>se uma letra estiver laranja, significa que ela existe na palavra mas não está no lugar certo</Box>
-                    <Box sx={{ gap: "2vw" }}>
-                        <Letter letter="T" />
-                        <Letter letter="R" correct />
-                        <Letter letter="E" />
-                        <Letter letter="T" />
-                        <Letter letter="A" />
-                    </Box>
-                    <Box>se ela estiver verde, significa que essa letra está no lugar certo</Box>
-                    <Box sx={{ gap: "2vw" }}>
-                        <Letter letter="T" />
-                        <Letter letter="R" />
-                        <Letter letter="E" partial multiple />
-                        <Letter letter="T" />
-                        <Letter letter="A" correct />
-                    </Box>
-                    <Box>se a borda da letra estiver azul, há mais de uma ocorrência dessa letra na palavra!</Box>
-                    <Box sx={{ gap: "2vw" }}>
-                        <Letter letter="T" correct multiple />
-                        <Letter letter="R" correct />
-                        <Letter letter="E" correct />
-                        <Letter letter="T" correct multiple />
-                        <Letter letter="A" correct />
-                    </Box>
-                    <Box>ao acertar a palavra, você ganha pontos baseado na quantidade de tentativas</Box>
+            <Box sx={{ flexDirection: "column", padding: "10vw", height: "85vh", overflowY: "auto", gap: "2vw" }}>
+                <Box sx={{ fontSize: "1.5rem", fontWeight: "bold", alignSelf: "center" }}>como jogar</Box>
+                <Box>o objetivo do jogo é descobrir qual é a palavra</Box>
+                <Box>digite uma palavra com a quantidade de letras equivalente para receber dicas</Box>
+                <Box sx={{ gap: "2vw" }}>
+                    <Letter letter="T" />
+                    <Letter letter="R" />
+                    <Letter letter="E" />
+                    <Letter letter="T" />
+                    <Letter letter="A" partial />
                 </Box>
+                <Box>se uma letra estiver laranja, significa que ela existe na palavra mas não está no lugar certo</Box>
+                <Box sx={{ gap: "2vw" }}>
+                    <Letter letter="T" />
+                    <Letter letter="R" correct />
+                    <Letter letter="E" />
+                    <Letter letter="T" />
+                    <Letter letter="A" />
+                </Box>
+                <Box>se ela estiver verde, significa que essa letra está no lugar certo</Box>
+                <Box sx={{ gap: "2vw" }}>
+                    <Letter letter="T" />
+                    <Letter letter="R" />
+                    <Letter letter="E" partial multiple />
+                    <Letter letter="T" />
+                    <Letter letter="A" correct />
+                </Box>
+                <Box>se a borda da letra estiver azul, há mais de uma ocorrência dessa letra na palavra!</Box>
+                <Box sx={{ gap: "2vw" }}>
+                    <Letter letter="T" correct multiple />
+                    <Letter letter="R" correct />
+                    <Letter letter="E" correct />
+                    <Letter letter="T" correct multiple />
+                    <Letter letter="A" correct />
+                </Box>
+                <Box>ao acertar a palavra, você ganha pontos baseado na quantidade de tentativas</Box>
             </Box>
         </SwipeableDrawer>
     )
