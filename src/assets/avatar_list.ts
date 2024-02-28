4a061fa191a3e832506a34e6148376702f09eda3
nandobfer <nandobfer@yahoo.com.br>
2024-02-28 17:48:42 -0300

    update: locally storing avatars

--- src/assets/avatar_list.ts
+++ src/assets/avatar_list.ts
@@ -1,25 +1,39 @@
-import ste from "../assets/Ste.jpg"
+import Barbara from "../assets/Barbara.webp"
+import Bea from "../assets/Bea.webp"
+import Bruno from "../assets/Bruno.webp"
+import Ellyham from "../assets/Ellyham.webp"
+import Fernando from "../assets/Fernando.webp"
+import Gimerson from "../assets/Gimerson.webp"
+import Guilherme from "../assets/Guilherme.webp"
+import Igor from "../assets/Igor.webp"
+import Israel from "../assets/Israel.webp"
+import Joana from "../assets/Joana.webp"
+import Lucas from "../assets/Lucas.webp"
+import Luiz from "../assets/Luiz.webp"
+import Mizael from "../assets/Mizael.webp"
+import Natalia from "../assets/Natália.webp"
+import Ste from "../assets/Ste.webp"
+import Stezinha from "../assets/Stersinha.webp"
+import Tainara from "../assets/Tainara.webp"
+import Vitoria from "../assets/Vitória.webp"
 
 export const avatar_list = [
-    "https://drive.google.com/uc?id=10lbCAxyWogCRldLcBf8VEX32qczfejs1", // ste
-    "https://drive.google.com/uc?id=10jppRkQi_AEx7Fq99qBG8AYbqB_27OOn", // nat
-    "https://drive.google.com/uc?id=10bv0TbCuaHltmgLUWahBvoZpdu-522iE", // miza
-    "https://drive.google.com/uc?id=10lru9JXzxFxlV_-amkYcRP_yFHuGRIX4", // joh
-    "https://drive.google.com/uc?id=10ey9SQH4xNlUqqR3AW-0KHdGeJ-kntn6", // gui
-    "https://drive.google.com/uc?id=10_h_46887y6Xv4-yNSkOC6robUe3DuJ1", // fer
-    "https://drive.google.com/uc?id=10egWKdAbZrP2KpTpSjJaM2Qf50HqyNyM", // elly
-    "https://drive.google.com/uc?id=10goobh4G9bWEprF_3SM1hZODwyl-XXuy", // bia
-    "https://drive.google.com/uc?id=118RBZ-7gTmoDOQRCXCPLkVx99HfoIKEk", // tai
-    "https://drive.google.com/uc?id=10rNZoLYzUrkbbr4Cbn6gmQhWwjJ99pHt", // stezinha
-    "https://drive.google.com/uc?id=11-y8ZGV9FW7tGpl8QxfYD3t2hVDaOSms", // luiz
-    "https://drive.google.com/uc?id=10rB58Zhibsqw6ncDLbzXuFDB4zfUgqpV", // lourenço
-    "https://drive.google.com/uc?id=11IJlYnSJMZm_8z8wjx9zHk9eOI2wkDzt", // lidia
-    "https://drive.google.com/uc?id=116XFajblbwmx8OTqtQnCSQal7wigK53H", // 07
-    "https://drive.google.com/uc?id=10y8k7yDjU4ib4K1h8UDG1iAPzHlsi11l", // igor
-    "https://drive.google.com/uc?id=10nxAhY_5_oMicEO3iku7IKsMoutAR_BJ", // gimerson
-    "https://drive.google.com/uc?id=11GV8pEjg8Thj9GYI6NWwZMefueazjT5V", // bruno
-    "https://drive.google.com/uc?id=114CQgrzt-Cf_Y_m6rHWe7dCdvDap_zqY", // barbara
-    "https://drive.google.com/uc?id=168CEF9PgceXOj9UkPJ9IDlXVwPBRCnit", // vitoria
-    "https://drive.google.com/uc?id=13ZLHiyTo8vfeVMQ-XYmRG2-nwFe3z5Oo", // lucas
-    "https://drive.google.com/uc?id=13YdqlZw3A7IifKkB0GRkKRkk80Yc0Qhk" // augusto
+    Barbara,
+    Bea,
+    Bruno,
+    Ellyham,
+    Fernando,
+    Gimerson,
+    Guilherme,
+    Igor,
+    Israel,
+    Joana,
+    Lucas,
+    Luiz,
+    Mizael,
+    Natalia,
+    Ste,
+    Stezinha,
+    Tainara,
+    Vitoria,
 ]
